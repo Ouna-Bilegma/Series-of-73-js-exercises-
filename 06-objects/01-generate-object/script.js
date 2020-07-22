d379c5f84aa5f054e316d1fb42d17e955de9ef7b
Ouna-Bilegma <ounochka13@gmail.com>
2020-07-22 12:19:05 +0200

    objects

---
+++ 06-objects/01-generate-object/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /06-objects/01-generate-object/script.js - 6.1: générer un object
+ *
+ * coded by leny@BeCode
+ * started at 08/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    var person = new Object();
+    person.firstName = "Ouna";
+    person.lastName = "Tsybdenova";
+    person.age = 27;
+    person.city = "Geraardsbergen";
+    person.country = "Belgium";
+    console.log(person);
+  })();
+})();
---
+++ 06-objects/04-from-entries/script.js
@@ -0,0 +1,24 @@
+/* becode/javascript
+ *
+ * /06-objects/04-from-entries/script.js - 6.4: fromEntries
+ *
+ * coded by leny@BeCode
+ * started at 08/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+  const keys = ["name", "species", "age", "gender", "color"];
+  const values = ["Skitty", "cat", 9, "female", "tabby"];
+  // your code here
+  document.getElementById("run").addEventListener("click", function () {
+    const returnedTarget = Object.assign(keys, values);
+    const object2 = Object.fromEntries(
+      Object.entries(returnedTarget).map(([key, val]) => [key, val])
+    );
+
+    console.log(object2);
+  })();
+})();
